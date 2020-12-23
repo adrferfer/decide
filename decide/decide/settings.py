e72582c9292887cb8dbca24b18450a4453923f04
adrferfer <adrferfer@alum.us.es>
2020-12-23 17:17:38 +0100

    Versión inicial para despliegue travis y heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -161,9 +161,10 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
-# Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
+
+BASEURL = 'http://https://adr-app-decide.herokuapp.com'
 
 try:
     from local_settings import *
@@ -180,3 +181,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
